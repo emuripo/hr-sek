e9d5a3b2ffe59c107b803881648bb5b76e709529
estebanmp14 <103215941+estebanmp14@users.noreply.github.com>
2024-10-24 19:32:56 -0600

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useContext } from 'react'; 
+import { useLocation } from 'react-router-dom'; // Importar useLocation
 import AuthContext from '../context/AuthContext';
 import '../styles/Header.css'; // Importar el archivo CSS
 
 function Header() {
   const { username, isAuthenticated } = useContext(AuthContext);
+  const location = useLocation(); // Obtener la ubicación actual
+
+  // Verificar si la ruta es '/login'
+  if (location.pathname === '/login') {
+    return null; // No renderizar el Header en la página de login
+  }
 
   return (
     <div className="header-container">
